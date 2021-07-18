b6578d359bc347eb365f52e0cb5375faa869aefa
GMX\AVT2CE744 <vikrant.sharma2@dhl.com>
2021-07-18 20:36:03 +0530

    react hook form updated

--- src/components/Meals/AddEditMeals.js
+++ src/components/Meals/AddEditMeals.js
@@ -1,9 +1,9 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
@@ -23,19 +23,13 @@ export default function AddEditMeal(props) {
     register,
     handleSubmit,
     reset,
+    control,
     formState: { errors },
   } = useForm();
 
-  const [defaultValues,setDefaultValues] = useState(null);
-
-  useEffect(()=>{
-    let _defaultValues = {};
+  useEffect(() => {
     reset();
-    if(props.meal_to_edit){
-      Object.assign(_defaultValues,props.meal_to_edit);
-    }
-    setDefaultValues(_defaultValues);
-  },[props.meal_to_edit])
+  }, [props.meal_to_edit]);
 
   const classes = useStyles();
 
@@ -46,77 +40,80 @@ export default function AddEditMeal(props) {
           {props.addEditText + " " + "Meal"}
         </DialogTitle>
         <DialogContent>
-          <form
-            className={classes.form}
-            onSubmit={handleSubmit(props.addEditMeal)}
-          >
+          <form className={classes.form} onSubmit={handleSubmit(props.addEditMeal)}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="meal"
-                  variant="outlined"
-                  fullWidth
-                  id="meal"
-                  label="Meal"
+                <Controller
                   name="meal"
-                  autoFocus
-                  placeholder="Meal Name"
-                  {...register("meal", {
-                    required: true,
-                    maxLength: 20,
-                  })}
-                  error={errors.meal ? true : false}
-                  helperText={
-                    (errors.meal && errors.meal.type === "maxLength" && (
-                      <span>Max length exceeded</span>
-                    )) ||
-                    (errors.meal && errors.meal.type === "required" && (
-                      <span>This is required</span>
-                    ))
-                  }
+                  control={control}
+                  defaultValue={props.meal_to_edit && props.meal_to_edit.meal}
+                  render={({
+                    field: { onChange, value },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      label="Meal"
+                      variant="outlined"
+                      fullWidth
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                      type="meal"
+                    />
+                  )}
+                  rules={{ required: "This is required" }}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  id="date"
-                  label="Date"
-                  type="date"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  defaultValue={defaultValues && defaultValues.date}
-                  {...register("date", { required: true })}
-                  error={errors.date ? true : false}
-                  helperText={
-                    errors.date &&
-                    errors.date.type === "required" && (
-                      <span>This is required</span>
-                    )
-                  }
+                <Controller
+                  name="date"
+                  control={control}
+                  defaultValue={props.meal_to_edit && props.meal_to_edit.date}
+                  render={({
+                    field: { onChange, value },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      label="Date"
+                      variant="outlined"
+                      fullWidth
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                      type="date"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  )}
+                  rules={{ required: "This is required" }}
                 />
               </Grid>
               <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  id="calories"
-                  label="Calories"
-                  type="number"
-                  autoComplete="calories"
-                  placeholder="Calories"
-                  {...register("calories", {
-                    required: true,
-                  })}
-                  defaultValue={defaultValues && defaultValues.calories}
-                  error={errors.calories ? true : false}
-                  helperText={
-                    errors.calories &&
-                    errors.calories.type === "required" && (
-                      <span>This is required</span>
-                    )
+                <Controller
+                  name="calories"
+                  control={control}
+                  defaultValue={
+                    props.meal_to_edit && props.meal_to_edit.calories
                   }
+                  render={({
+                    field: { onChange, value },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      label="Calories"
+                      fullWidth
+                      variant="outlined"
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                      type="number"
+                    />
+                  )}
+                  rules={{ required: "This is required" }}
                 />
               </Grid>
             </Grid>
